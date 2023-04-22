3af5b670f142981d9577baac9b93393f8680dd96
Sagar Dighe <sagasoftech@gmail.com>
2023-04-22 17:46:30 -0500

    Initial version of Sprint Boot tutorial

---
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Course;
+
+@RestController
+public class CourseController {
+
+	@RequestMapping("/courses")
+	public List<Course> retriveAllCourses(){
+		return Arrays.asList(
+			new Course(1, "Learn AWS", "AWS"),
+			new Course(2, "Learn Java", "Oracle")
+		);
+	}
+}
