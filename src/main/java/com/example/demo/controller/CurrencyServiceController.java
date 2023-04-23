6a3e85a10a93478c5956ca714c618c815deb26d0
Sagar Dighe <sagasoftech@gmail.com>
2023-04-23 00:43:23 -0500

    Setting up default ConfigurationProperties

---
+++ src/main/java/com/example/demo/controller/CurrencyServiceController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.config.CurrencyServiceConfiguration;
+
+@RestController
+public class CurrencyServiceController {
+
+	@Autowired
+	private CurrencyServiceConfiguration configuration;
+	
+	@RequestMapping("/currency")
+	public CurrencyServiceConfiguration retriveAllCourses(){
+		return configuration;
+	}
+}
