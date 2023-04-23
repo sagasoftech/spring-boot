535844040a8ff4981de38f11452b10e0a16ffb3a
Sagar Dighe <sagasoftech@gmail.com>
2023-04-23 14:11:45 -0500

    Path veriable usage

--- src/main/java/com/example/demo/restful/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/restful/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.demo.restful.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-bean")
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Welcome to the world of Bean");
+	}
+	
+	@GetMapping(path = "hello-bean-param/{name}")
+	public HelloWorldBean getPersonalMessage(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Welcome to the world of Bean, %s", name));
 	}
 }
