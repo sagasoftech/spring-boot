9489f3d42a449d60be8f434356a8b99047451731
Sagar Dighe <sagasoftech@gmail.com>
2023-04-23 03:40:53 -0500

    Restful API response in Bean

--- src/main/java/com/example/demo/restful/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/restful/controller/HelloWorldController.java
@@ -2,8 +2,9 @@ package com.example.demo.restful.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.restful.model.HelloWorldBean;
 
 @RestController
 @RequestMapping("/restful")
@@ -18,5 +19,10 @@ public class HelloWorldController {
 	@GetMapping(path = "/")
 	public String helloWorld() {
 		return "Welcome to first Restful endpoint";
+	}
+	
+	@GetMapping(path = "/hello-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Welcome to the world of Bean");
 	}
 }
