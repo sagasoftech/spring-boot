7b7ed2e291083e65bc3285a3c635d783229e9cb4
Sagar Dighe <sagasoftech@gmail.com>
2023-04-23 19:15:12 -0500

    Use autowire to auto-detect Component

--- src/main/java/com/example/demo/restful/controller/UserResource.java
+++ src/main/java/com/example/demo/restful/controller/UserResource.java
@@ -2,6 +2,7 @@ package com.example.demo.restful.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,11 +17,8 @@ import com.example.demo.restful.model.User;
 @RequestMapping("/restful")
 public class UserResource {
 
+	@Autowired
 	private UserDaoService service;
-	
-	public UserResource(UserDaoService service) {
-		this.service = service;
-	}
 	
 	/*
 	 * GET - Retrieve details of Resources
