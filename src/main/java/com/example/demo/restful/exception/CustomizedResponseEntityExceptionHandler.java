906a6b8f41a1d7c15f96aa8f3ab5fd857340b1d9
Sagar Dighe <sagasoftech@gmail.com>
2023-04-24 01:14:24 -0500

    Build customized response for validation of REST API

--- src/main/java/com/example/demo/restful/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/demo/restful/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,8 +2,11 @@ package com.example.demo.restful.exception;
 
 import java.time.LocalDateTime;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -37,5 +40,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
 		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+	}
+	
+	/*
+	 * This method will handle all the request having invalid method argument
+	 * for which validation is enabled
+	 */
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+		ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<Object>(errorDetails, HttpStatus.NOT_FOUND);
 	}
 }
