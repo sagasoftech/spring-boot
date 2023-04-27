8ef5ec4e4f1e654b15727767524cc42aee44a92d
Sagar Dighe <sagasoftech@gmail.com>
2023-04-27 14:15:47 -0500

    Spring Data JPA demo for Select, Insert and Delete

---
+++ src/main/java/com/example/demo/springdatajpa/StorySpringDataJpaCommandLineRunner.java
@@ -0,0 +1,26 @@
+package com.example.demo.springdatajpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StorySpringDataJpaCommandLineRunner implements CommandLineRunner{
+
+	@Autowired
+	private StorySpringDataJpaRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		repository.save(new Story(1, "Java", "Herbert Schildt"));
+		repository.save(new Story(2, "Docker", "Docker Foundation"));
+		repository.save(new Story(3, "Kubernetes", "K8s Foundation"));
+		
+		repository.deleteById(2l);
+		
+		System.out.println("Story: " + repository.findById(1l));
+		System.out.println("Story: " + repository.findById(3l));
+	}
+
+	
+}
