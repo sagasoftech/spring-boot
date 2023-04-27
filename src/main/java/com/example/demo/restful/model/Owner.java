e96dfdbc1f81a1c58b0d09472821539a15096b8d
Sagar Dighe <sagasoftech@gmail.com>
2023-04-27 17:56:01 -0500

    New Entity class representing table OWNER

---
+++ src/main/java/com/example/demo/restful/model/Owner.java
@@ -0,0 +1,58 @@
+package com.example.demo.restful.model;
+
+import java.time.LocalDate;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Past;
+
+@Entity
+public class Owner {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Length(min=2, message = "Name should have atleast two charactors")
+	private String name;
+	
+	@Past(message = "Birth Date should be in past")
+	private LocalDate birthDate;
+	
+	public Owner(int id, String name, LocalDate birthDate) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+	
+	@Override
+	public String toString() {
+		return "Owner [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
+	}
+
+}
