48d221b704e6d388a9e2ba51750addcaee90d8b5
Sagar Dighe <sagasoftech@gmail.com>
2023-04-27 20:24:22 -0500

    Spring Boot JPA with REST API: Get owner by ID

--- src/main/java/com/example/demo/restful/controller/OwnerJpaResource.java
+++ src/main/java/com/example/demo/restful/controller/OwnerJpaResource.java
@@ -1,12 +1,15 @@
 package com.example.demo.restful.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.restful.exception.UserNotFoundException;
 import com.example.demo.restful.model.Owner;
 import com.example.demo.restful.repository.OwnerRepository;
 
@@ -22,5 +25,13 @@ public class OwnerJpaResource {
 		return repository.findAll();
 	}
 	
-	
+	@GetMapping("/owner-jpa/{id}")
+	public Optional<Owner> findUser(@PathVariable Long id) {
+		Optional<Owner> owner = repository.findById(id);
+		
+		if(owner.isEmpty()) {
+			throw new UserNotFoundException("id: "+ id);
+		}
+		return owner;
+	}
 }
